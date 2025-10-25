452bdf2ab9d72d5e183e5dd3cdbd91cf93478b30
Darya Melnik <daryamelnik@daryas-macbook-air-2.home>
2025-10-25 09:43:25 +0200

    fixed remark

--- tests/test_scooter_api.py
+++ tests/test_scooter_api.py
@@ -1,32 +1,7 @@
 import requests
-import random
-import string
 import pytest
 import allure
-
-BASE_URL = 'https://qa-scooter.praktikum-services.ru/api/v1'
-
-def generate_random_string(length):
-    letters = string.ascii_lowercase
-    random_string = ''.join(random.choice(letters) for i in range(length))
-    return random_string
-
-def register_new_courier_and_return_login_password():
-    login = generate_random_string(10)
-    password = generate_random_string(10)
-    first_name = generate_random_string(10)
-
-    payload = {
-        "login": login,
-        "password": password,
-        "firstName": first_name
-    }
-
-    response = requests.post(f'{BASE_URL}/courier', data=payload)
-
-    if response.status_code == 201:
-        return [login, password, first_name]
-    return []
+from .helpers import generate_random_string, register_new_courier_and_return_login_password, BASE_URL
 
 @allure.feature('Courier API')
 class TestCourierAPI:
